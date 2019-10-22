ac6f821e6c1ddb0558260efcd8b94e2e57f293cf
Eliseu Codinhoto <zeucxb@gmail.com>
2019-10-22 18:55:54 -0300

    feat(internacionalization): Add support to internacionalization

--- lib/app.dart
+++ lib/app.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
-
-import 'package:remessa_app/theme.dart';
-import 'package:remessa_app/widgets/widgets.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:remessa_app/helpers/i18n.dart';
+import 'package:remessa_app/widgets/tab_controller/tab_controller_widget.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Calculadora de compra internacional',
-      debugShowCheckedModeBanner: false,
-      theme: AppTheme().theme,
+      title: 'Remessa Online',
+      localizationsDelegates: [
+        const I18nDelegate(),
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+      ],
+      supportedLocales: [
+        const Locale('en', ''),
+        const Locale('pt', ''),
+      ],
+      localeResolutionCallback:
+          (Locale locale, Iterable<Locale> supportedLocales) {
+        if (locale == null) return supportedLocales.first;
+
+        for (Locale supportedLocale in supportedLocales) {
+          if (supportedLocale.languageCode == locale.languageCode ||
+              supportedLocale.countryCode == locale.countryCode) {
+            return supportedLocale;
+          }
+        }
+
+        return supportedLocales.first;
+      },
       home: TabControllerWidget(),
     );
   }
--- lib/widgets/tab_controller/tab_controller_widget.dart
+++ lib/widgets/tab_controller/tab_controller_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:remessa_app/helpers/i18n.dart';
 
 import 'package:remessa_app/screens/calculator/calculator_screen.dart';
 import 'package:remessa_app/widgets/tab_controller/bloc/bloc.dart';
@@ -7,37 +8,51 @@ import 'package:zendesk_chat/zendesk_chat.dart';
 
 class TabContent {
   final String title;
+  final IconData iconData;
   final Widget widget;
 
   TabContent({
     @required this.title,
+    @required this.iconData,
     @required this.widget,
   })  : assert(title != null),
+        assert(iconData != null),
         assert(widget != null);
 }
 
-class TabControllerWidget extends StatelessWidget {
-  final _tabs = <TabContent>[
-    TabContent(
-      title: 'Ajuda',
-      widget: CalculatorScreen(),
-    ),
-    TabContent(
-      title: 'Dashboard',
-      widget: CalculatorScreen(),
-    ),
-    TabContent(
-      title: 'Chat',
-      widget: Container(),
-    ),
-  ];
+class TabControllerWidget extends StatefulWidget {
+  @override
+  _TabControllerWidgetState createState() => _TabControllerWidgetState();
+}
 
+class _TabControllerWidgetState extends State<TabControllerWidget> {
   final _tabControllerBloc = TabControllerBloc();
+
   final _zendeskChat =
       ZendeskChat(accountKey: 'MyqHVBcMtviH16mTlMyl0AlpYYsJLS2i');
 
   @override
   Widget build(BuildContext context) {
+    final i18n = I18n.of(context);
+
+    final _tabs = <TabContent>[
+      TabContent(
+        title: i18n.trans('help'),
+        iconData: Icons.help,
+        widget: CalculatorScreen(),
+      ),
+      TabContent(
+        title: i18n.trans('dashboard'),
+        iconData: Icons.dashboard,
+        widget: CalculatorScreen(),
+      ),
+      TabContent(
+        title: i18n.trans('chat'),
+        iconData: Icons.chat,
+        widget: Container(),
+      ),
+    ];
+
     return BlocBuilder<TabControllerBloc, TabControllerState>(
       bloc: _tabControllerBloc,
       builder: (context, state) => Scaffold(
@@ -50,27 +65,21 @@ class TabControllerWidget extends StatelessWidget {
           onTap: (int index) {
             onTabTapped(context, index);
           },
-          items: [
-            BottomNavigationBarItem(
-              icon: Icon(Icons.help),
-              title: Text('Ajuda'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.dashboard),
-              title: Text('Dashboard'),
-            ),
-            BottomNavigationBarItem(
-              icon: Icon(Icons.chat),
-              title: Text('Chat'),
-            )
-          ],
+          items: _tabs
+              .map(
+                (tab) => BottomNavigationBarItem(
+                  icon: Icon(tab.iconData),
+                  title: Text(tab.title),
+                ),
+              )
+              .toList(),
         ),
       ),
     );
   }
 
   void onTabTapped(BuildContext context, int index) {
-    if (index == _tabs.length - 1) {
+    if (index == 2) {
       _zendeskChat.openChatScreen(
         context,
         ChatSettings(
