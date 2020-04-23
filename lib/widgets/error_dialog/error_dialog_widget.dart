0f62019c849ef034604a2299c6dc1f3387f30f43
Eliseu Codinhoto <zeucxb@gmail.com>
2020-04-23 18:41:52 -0300

    fix(error-dialog): Error dialog refac

--- lib/widgets/error_dialog/error_dialog_widget.dart
+++ lib/widgets/error_dialog/error_dialog_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
+import 'package:remessa_app/presentation/remessa_icons_icons.dart';
 import 'package:remessa_app/style/colors.dart';
 
 class ErrorDialog extends StatelessWidget {
@@ -17,8 +18,8 @@ class ErrorDialog extends StatelessWidget {
         super(key: key);
 
   _closeDialog() {
-    if (previousStatusBarWhiteForeground != null) {
-      FlutterStatusbarcolor.setStatusBarWhiteForeground(false);
+    if (previousStatusBarWhiteForeground) {
+      FlutterStatusbarcolor.setStatusBarWhiteForeground(true);
     }
 
     closeFunction();
@@ -35,13 +36,19 @@ class ErrorDialog extends StatelessWidget {
       child: SafeArea(
         child: Material(
           child: Container(
-            padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+            padding: EdgeInsets.symmetric(vertical: 18, horizontal: 20),
             color: StyleColors.SUPPORT_DANGER_40,
             width: double.infinity,
             child: Row(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
-                Icon(Icons.warning),
+                Padding(
+                  padding: const EdgeInsets.only(top: 2),
+                  child: Icon(
+                    RemessaIcons.warning,
+                    size: 18,
+                  ),
+                ),
                 Expanded(
                   child: Container(
                     padding: EdgeInsets.symmetric(horizontal: 20),
@@ -51,9 +58,19 @@ class ErrorDialog extends StatelessWidget {
                     ),
                   ),
                 ),
-                IconButton(
-                  icon: Icon(Icons.close),
-                  onPressed: _closeDialog,
+                GestureDetector(
+                  child: Container(
+                    padding: EdgeInsets.all(3),
+                    decoration: BoxDecoration(
+                      color: StyleColors.SUPPORT_NEUTRAL_10.withOpacity(0.3),
+                      borderRadius: BorderRadius.circular(100),
+                    ),
+                    child: Icon(
+                      Icons.close,
+                      size: 18,
+                    ),
+                  ),
+                  onTap: _closeDialog,
                 ),
               ],
             ),
